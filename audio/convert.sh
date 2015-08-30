8f985e5c5a99ebc00f580721502c43abf69e8793
Fiskr <bjf092@gmail.com>
2015-08-30 16:34:32 -0400

    adding bash script to convert file types

---
+++ audio/convert.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+if [ $# -gt "0" ]; then
+  if [[ $1 =~ help ]]; then
+    echo "Usage: $0 [directory] [from extension, e.g. flac] [to extension, e.g. wav]"
+    exit 1
+  fi
+  directory="$1"
+else
+  read -p "Which directory are the files in which you would like converted? > " directory
+fi
+
+if [ $# -gt "1" ]; then
+  fromType="$2"
+ else
+   read -p "What format would you like to convert from? (e.g. flac) > " fromType
+fi
+
+if [ $# -gt "2" ]; then
+  toType="$3"
+else
+  read -p "What format would you like to convert to? (e.g. wav) > " toType
+fi
+
+function clearWhiteSpace(){
+  if find . -name "* *" -type d; then
+   echo "DEBUG: clearWhiteSpace() found: $(find . -name "* *" -type d)"
+   find . -name "* *" -type d | rename 's/ /_/g'
+  fi
+  if find . -name "* *" -type f; then
+    echo "DEBUG: clearWhiteSpace() found: $(find . -name "* *" -type f)"
+    find . -name "* *" -type f | rename 's/ /_/g'
+  fi
+}
+
+if [[ $directory =~ " " ]];then
+  cd "$directory"
+  cd ..
+  clearWhiteSpace
+  directory="$(echo "$directory" | sed 's/ /_/g')"
+fi
+
+echo "Converting from $fromType to $toType"
+
+cd "$directory"
+echo "Changed directory to $directory"
+
+mkdir -p $toType
+echo "Made new folder, $toType"
+
+for file in ./*.$fromType; do
+  filename=$(basename "$file")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  echo $filename
+  #ffmpeg -i "$filename.$fromType" -f $toType "$toType/$filename.$toType"
+  if [[ $fromType == "flac" ]]; then
+    if which flac; then
+      echo "flac -d $filename.flac"
+      flac -d "$filename.$fromType"
+    else
+      >&2 echo "You need to install flac to convert flac files: "
+      >&2 echo "sudo apt-get install flac"
+      exit 1
+    fi
+  elif [[ $toType == "wav" ]]; then
+    if which mpg123; then
+      mpg123 -w $filename.$toType $filename.$fromType
+    else
+      >&2 echo "You need to install mpg123 to convert to $toType: "
+      >&2 echo "sudo apt-get install mpg123"
+      exit 1
+    fi
+  fi
+done
+
+mv *.$toType $toType
