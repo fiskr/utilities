b728b80d7d38620b4c9d844f1d9cc6f94e6de216
Brandon Foster <bfoster@scrippsnetworks.com>
2016-11-18 13:24:39 -0500

    Updating video device location and check for processes before killing them

--- camera/record.sh
+++ camera/record.sh
@@ -10,13 +10,27 @@ videoBitrate="2500" # for 720p
 # For More Information about bitrate: http://www.ezs3.com/public/What_bitrate_should_I_use_when_encoding_my_video_How_do_I_optimize_my_video_for_the_web.cfm
 
 fileLocation="/media/serotonin/SeagateBackup/video/security/"
+videoDevice="/dev/video0"
 
+function killVlcInstances() {
+  if [[ $(ps -ef | grep -i vlc | grep -v grep | wc -l) != 0 ]]; then
 
-# kill any recording instances
-ps -ef | grep -i vlc | grep -v grep | awk '{print $2}' | xargs kill
+    ps -ef | grep -i vlc | grep -v grep | awk '{print $2}' | xargs kill
+
+    sleep 10
+
+    killVlcInstances
+  fi
+}
+
 # delete old video files
-find $fileLocation -name "*$fileExtension" -mtime +3 | xargs rm -f
+find $fileLocation -type f -mtime +3 | xargs rm -f
 
-sleep 5
+# stop recording
+killVlcInstances
 
-cvlc v4l2:// :v4l2-dev=/dev/video2 --sout='#transcode{vcodec='$videoCodec', vb='$videoBitrate'}:std{access=file, mux='$muxCodec', dst='$fileLocation$(date --iso-8601=seconds)$fileExtension'}' --no-audio &
+echo "" 2>&1
+echo "" 2>&1
+echo "Beginning of Recording: $(date --iso-8601=minutes)" 2>&1
+
+export DISPLAY=:0; cvlc v4l2:// :v4l2-dev=$videoDevice --sout='#transcode{vcodec='$videoCodec', vb='$videoBitrate'}:std{access=file, mux='$muxCodec', dst='$fileLocation$(date --iso-8601=seconds)$fileExtension'}' --no-audio &
